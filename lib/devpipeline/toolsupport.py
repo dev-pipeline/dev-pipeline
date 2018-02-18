12470d3dcb50c2f2491f239219e1e56fe207b449
Stephen Newell <sjnewell@gmail.com>
2018-02-17 20:28:22 -0700

    Consolidate common code

--- lib/devpipeline/toolsupport.py
+++ lib/devpipeline/toolsupport.py
@@ -16,21 +16,27 @@ def tool_builder(component, key, tool_map):
         raise Exception("{} does not specify {}".format(component._name, key))
 
 
-def args_builder(prefix, component, args_dict, val_found_fn):
-    pattern = re.compile(R"^{}\.".format(prefix))
+def _args_helper(pattern, component, fn):
     for key, value in component.items():
         m = pattern.match(key)
         if m:
-            real_key = key[m.end():]
-            hit = args_dict.get(real_key)
-            if hit:
-                val_found_fn(value, hit)
+            fn(key, value, m)
+
+
+def args_builder(prefix, component, args_dict, val_found_fn):
+    def call_fn(key, value, m):
+        real_key = key[m.end():]
+        hit = args_dict.get(real_key)
+        if hit:
+            val_found_fn(value, hit)
+
+    pattern = re.compile(R"^{}\.".format(prefix))
+    _args_helper(pattern, component, call_fn)
 
 
 def flex_args_builder(prefix, component, args_dict, val_found_fn):
     for key, flex_fn in args_dict.items():
         pattern = re.compile(R"^{}\.({})\.?".format(prefix, key))
-        for option, value in component.items():
-            m = pattern.match(option)
-            if m:
-                val_found_fn(value, option[m.end():], flex_fn)
+        _args_helper(pattern, component,
+                     lambda k, v, m:
+                     val_found_fn(v, k[m.end():], flex_fn))
