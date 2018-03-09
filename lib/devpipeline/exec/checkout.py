886d61ec9159d3ad23779170f473440fe5dc87cb
Triston Whetten <crazyorc@users.noreply.github.com>
2018-03-09 15:28:30 -0700

    exec/checkout.py: address pylint3 issues

--- lib/devpipeline/exec/checkout.py
+++ lib/devpipeline/exec/checkout.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""This modules does the checkout of code from SCM."""
 
 import devpipeline.scm.scm
 import devpipeline.common
 
 
 def main(args=None):
+    # pylint: disable=missing-docstring
     checkout = devpipeline.common.TargetTool([
         devpipeline.scm.scm.scm_task
     ], prog="dev-pipeline checkout", description="Checkout repositories")
