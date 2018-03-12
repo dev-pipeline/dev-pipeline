f3d84e8981191500d6ef9e83a29bf7329c5e40d9
Stephen Newell <sjnewell@gmail.com>
2018-03-11 23:39:44 -0600

    Fix resolve crash when not running against the full set

--- lib/devpipeline/resolve.py
+++ lib/devpipeline/resolve.py
@@ -3,14 +3,26 @@
 
 
 def _build_dep_data(targets, components):
-    """Returns dependency data for a set of targets. An exception will be raised if a target does
-    not have component configuration available."""
-    counts = {target: 0 for target in targets}
-    reverse_deps = {target: list() for target in targets}
+    """
+    Returns dependency data for a set of targets. An exception will be raised
+    if a target does not have component configuration available.
+    """
+    counts = {}
+    reverse_deps = {}
+
+    def _add_reverse_deps(package, dependencies):
+        nonlocal reverse_deps
+
+        for dependency in dependencies:
+            if dependency not in reverse_deps:
+                reverse_deps[dependency] = []
+            reverse_deps[dependency].append(package)
 
     def get_deps_from_component(component):
-        """Given a component, return a list of dependencies. An empty list will be returned for
-        components with no dependencies."""
+        """
+        Given a component, return a list of dependencies. An empty list will
+        be returned for components with no dependencies.
+        """
         dependencies = component.get("depends")
         if dependencies:
             return list(x.strip() for x in dependencies.split(','))
@@ -30,8 +42,7 @@ def _build_dep_data(targets, components):
         if current not in processed_targets:
             component_deps = get_deps_from_component(components[current])
             counts[current] = len(component_deps)
-            for dep in component_deps:
-                reverse_deps[dep].append(current)
+            _add_reverse_deps(current, component_deps)
 
             # process component dependencies as well
             to_be_processed += component_deps
@@ -41,10 +52,13 @@ def _build_dep_data(targets, components):
 
 
 def order_dependencies(targets, components):
-    """Given a list of targets and component configurations, return a list of targets in build
-    order. The list order guarantees every target's dependencies are included prior to that target.
+    """
+    Given a list of targets and component configurations, return a list of
+    targets in build order. The list order guarantees every target's
+    dependencies are included prior to that target.
 
-    An exception will be thrown if dependencies can't be resolved."""
+    An exception will be thrown if dependencies can't be resolved.
+    """
     def get_resolved_targets(counts):
         """Get a list of targets with a dependency count of 0."""
         resolved_targets = list()
@@ -55,7 +69,9 @@ def order_dependencies(targets, components):
         return resolved_targets
 
     def remove_reverse_deps(target, reverse_deps, counts):
-        """Remove a given target from the dependency list of all other targets."""
+        """
+        Remove a given target from the dependency list of all other targets.
+        """
         for rev_deps in reverse_deps[target]:
             counts[rev_deps] -= 1
         del reverse_deps[target]
@@ -65,8 +81,8 @@ def order_dependencies(targets, components):
     while counts:
         resolved_targets = get_resolved_targets(counts)
 
-        # Every pass must resolve at least one target. An exception is raised if
-        # no targets are resolved to avoid an infinte loop.
+        # Every pass must resolve at least one target. An exception is raised
+        # if no targets are resolved to avoid an infinte loop.
         if not resolved_targets:
             raise Exception("Resolve error")
 
