0a6814758c74ab0390428a90529eb4c56988f03b
Stephen Newell <sjnewell@gmail.com>
2018-04-01 11:20:22 -0600

    Fix issues detected by codeclimate

--- lib/devpipeline/config/env.py
+++ lib/devpipeline/config/env.py
@@ -9,32 +9,32 @@ import devpipeline.config.profile
 
 _ENV_PATTERN = re.compile(R"env.(\w+)")
 
+def _add_override(values, adjustments):
+    for key in values:
+        match = _ENV_PATTERN.match(key)
+        if match:
+            adjustments[match.group(1)] = None
+
 
 def _source_profiles(config_map, adjustments):
-    def _add_override(profile_name, profile_config):
+    def _add_override_helper(profile_name, profile_config):
         # pylint: disable=unused-argument
-        for key in profile_config:
-            match = _ENV_PATTERN.match(key)
-            if match:
-                adjustments[match.group(1)] = None
+        _add_override(profile_config, adjustments)
 
     devpipeline.config.profile.apply_profiles(
         config_map["current_config"],
-        config_map, _add_override)
+        config_map, _add_override_helper)
 
 
 def _source_overrides(config_map, adjustments):
-    def _add_override(overrides, values):
+    def _add_override_helper(overrides, values):
         # pylint: disable=unused-argument
-        for key in values:
-            match = _ENV_PATTERN.match(key)
-            if match:
-                adjustments[match.group(1)] = None
+        _add_override(values, adjustments)
 
     devpipeline.config.override.apply_overrides(
         config_map["current_config"],
         config_map["current_target"],
-        config_map, _add_override)
+        config_map, _add_override_helper)
 
 
 _SOURCE_FUNCTIONS = [
@@ -51,17 +51,20 @@ def get_env_list(config_map):
 
 
 def create_environment(config_map):
+    def _apply_override(adjustment, ret):
+        upper_key = adjustment.upper()
+        new_value = devpipeline.config.modifier.modify_everything(
+            ret.get(upper_key), config_map,
+            "env.{}".format(adjustment), os.pathsep)
+        if new_value:
+            ret[upper_key] = new_value
+        else:
+            ret.pop(upper_key, None)
+
     env_adjustments = get_env_list(config_map)
     if env_adjustments:
         ret = os.environ.copy()
         for adjustment in env_adjustments:
-            upper_key = adjustment.upper()
-            new_value = devpipeline.config.modifier.modify_everything(
-                ret.get(upper_key), config_map,
-                "env.{}".format(adjustment), os.pathsep)
-            if new_value:
-                ret[upper_key] = new_value
-            else:
-                ret.pop(upper_key, None)
+            _apply_override(adjustment, ret)
         return ret
     return os.environ
