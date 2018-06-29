c3e0f57035decf364c7812160d6e667a55f2f41a
Stephen Newell <sjnewell@gmail.com>
2018-06-28 21:23:38 -0600

    Version bump

--- lib/devpipeline/version.py
+++ lib/devpipeline/version.py
@@ -3,7 +3,7 @@
 """Version string for module"""
 
 MAJOR = 0
-MINOR = 1
+MINOR = 2
 PATCH = 0
 
 ID = (MAJOR << 24) | (MINOR << 16) | (PATCH << 8)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dev-pipeline",
-    version="0.1.0",
+    version="0.2.0",
     package_dir={
         "": "lib"
     },
