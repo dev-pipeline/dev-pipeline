6fef6b82d43b9b8d2db1278d152ed811e4bc3b34
Stephen Newell <sjnewell@gmail.com>
2019-01-24 22:00:55 -0700

    Fix code climate issue

--- lib/devpipeline/driver.py
+++ lib/devpipeline/driver.py
@@ -9,14 +9,16 @@ import sys
 import devpipeline
 
 
-def _do_help():
+def _do_help(args):
+    del args
     print("usage: dev-pipeline tool [tool args]")
     print("Arguments:")
     print("\t--help\tDisplay this help")
     print("\t--list\tDisplay available tools")
 
 
-def _do_list():
+def _do_list(args):
+    del args
     for tool in sorted(devpipeline.TOOLS):
         print("{} - {}".format(tool, devpipeline.TOOLS[tool][1]))
 
@@ -24,21 +26,24 @@ def _do_list():
 _EX_TOOLS = {"--help": _do_help, "--list": _do_list}
 
 
+def _find_tool():
+    tool = devpipeline.TOOLS.get(sys.argv[1])
+    if tool:
+        return tool[0]
+    return _EX_TOOLS.get(sys.argv[1])
+
+
 def main():
     # pylint: disable=missing-docstring
     if len(sys.argv) > 1:
-        tool = devpipeline.TOOLS.get(sys.argv[1])
+        tool = _find_tool()
         if tool:
-            tool[0](sys.argv[2:])
+            tool(sys.argv[2:])
         else:
-            tool = _EX_TOOLS.get(sys.argv[1])
-            if tool:
-                tool()
-            else:
-                print("{} isn't an available tool".format(sys.argv[1]))
-                sys.exit(1)
+            print("{} isn't an available tool".format(sys.argv[1]), file=sys.stderr)
+            sys.exit(1)
     else:
-        _do_help()
+        _do_help(None)
 
 
 if __name__ == "__main__":
